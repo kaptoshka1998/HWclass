ac4fee7e64f9d04a1c0e7be2f76bb700a316a38e
Darya <allexxika@yandex.ru>
2022-04-21 20:41:49 +0300

    первый коммит

--- src/ru/skypro/Main.java
+++ src/ru/skypro/Main.java
@@ -2,7 +2,16 @@ package ru.skypro;
 
 public class Main {
 
+
     public static void main(String[] args) {
-	// write your code here
+        Author firstAuthor = new Author("Albert", "Comuy");
+        Book firstBook = new Book("In vino veritas", 1998, firstAuthor);
+        System.out.println("'" + firstBook.getName() + "'" + " " + firstBook.getAuthor().firstName + " " + firstBook.getAuthor().secondName+ " " + firstBook.getYear());
+        firstBook.setYear(1997);
+        System.out.println("firstBook.getYear() = " + firstBook.getYear());
+        Author secondAuthor = new Author("Ludvig Van","Bethoven");
+        Book secondBook = new Book("Crazy frog", 2015, secondAuthor);
+        System.out.println("'" + secondBook.getName() + "'" + " " + secondBook.getAuthor().firstName + " " + secondBook.getAuthor().secondName+ " " + secondBook.getYear());
+
     }
 }
